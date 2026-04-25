cd24dd0e470f84085fd36a6410e0ca57d21c1d5c
karawaqqe <146225027+karawaqqe@users.noreply.github.com>
2026-04-25 21:44:10 +0200

    s

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -1,11 +1,11 @@
 import { Component } from "react";
-import "./Modal.css";
+import s from "./modal.module.css";
 
 export class Modal extends Component {
     render() {
         return (
-            <div className="backdrop">
-                <div className="modal">
+            <div className={s.backdrop}>
+                <div className={s.modal}>
                     {this.props.children}
                 </div>
             </div>
